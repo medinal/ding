c52254f6e5fbe4a82348d4e508115626ee4028cf
Will Kaspar <Will@Aang.local>
2017-02-16 18:19:00 -0800

    user dropdown added with basic functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+var $currentId;
+
 
 $(document).ready(function(){
   // jQuery Variables
@@ -7,29 +9,70 @@ $(document).ready(function(){
   var enrolledCourses = $('.enrolled-courses');
 
 
+  // USER LIST AJAX
+  $.ajax({
+    method: "GET",
+    url: "/users",
+    success: function(res){
+      onSuccess(res);
+      res.forEach(function RenderAllUsers(user){
+        renderUser(user);
+      });
+    }
+  }); // closes '/users' ajax
+
+  // CLASS LIST AJAX
   $.ajax({
     method: "GET",
     url: "/courses",
     success: function(res){
-        onSuccess(res);
         res.forEach(function RenderAllCourses(course){
           renderCourse(course, allCourses);
         });
+        res.forEach(function RenderCoursesTeaching(course){
+          filterByTeacher();
+          //renderCourse(course, teachCourses);
+        })
     },
     error: onError
-  })
+  }); // closes '/courses' ajax
 
-});
+
+  // TODO : FILTER COURSES BY TEACHER
+  var filterByUser = function(){
+    $currentId = $(this).attr('data-id');
+    console.log('currentId: ', $currentId);
+    if($currentId === res.teacher._id){
+      console.log(res.name);
+      //renderCourse(course, teachCourses);
+    }
+  }
+  $('.user-dropdown').on('click', '.dropdown-item', filterByUser);
+
+
+});  // closes (document).ready()
+
+
+
+
 
 // Runs on successful ajax call
 var onSuccess = function(res){
   console.log('good job, ajax!');
   console.log(res);
 }
+
 // Runs on erronous ajax call
 var onError = function(){
   console.log('try again, ajax');
 }
+
+// Renders User to Dropdown Menu
+var renderUser = function(res){
+  $('.user-dropdown').append(`
+    <a class="dropdown-item" href="#" data-id="${res._id}">${res.name}</a>`)
+  }
+
 // Renders courses to 'views/index.html'
 var renderCourse = function(res, div){
   $(div).prepend(`
@@ -43,7 +86,7 @@ var renderCourse = function(res, div){
           <p><strong>Description:</strong>  ${res.description}</p>
           <p><strong>Capacity:</strong>  ${res.capacity}</p>
         </div>
-        <div class="btn-group">
+        <div class="btn-group class-btn-group">
           <button type="button" class="edit-btn btn btn-default btn-md" dataId=''>edit</button>
           <button type="button" class="trash-btn btn btn-default btn-md" dataId=''>trash</button>
         </div>
